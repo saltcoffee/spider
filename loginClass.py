c05eb765968443780200a3b33dc085d863263a52
我回来啦 <loveplatolovecs@gmail.com>
2013-12-04 12:50:16 +0800

    add headers

--- loginClass.py
+++ loginClass.py
@@ -9,7 +9,7 @@ import cookielib
 import time
 
 class LoginDouban(object):
-	"""docstring for ClassName""" m
+	"""docstring for ClassName""" 
 	def __init__(self, email, password):
 		#super(ClassName, self).__init__()
 		#self.response = response
@@ -21,6 +21,20 @@ class LoginDouban(object):
 				"form_password":password,
 				"source":"index_nav"
 		}
+
+		head = {
+			"Connection":'keep-alive',
+			"HOST":'www.douban.com',
+			"Referer":'http://www.baidu.com/',
+			"User_Agent":'Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0'
+		}
+
+		req = urllib2.Request(self.url)
+
+
+		for  item in head:
+			req.add_header(item, head[item])
+
 		self.response = self.opener.open(self.url, urllib.urlencode(self.data))
 
 	def login_douban(self):
@@ -43,8 +57,27 @@ class LoginDouban(object):
 		        #登录成功
 		        if self.response.geturl() == "http://www.douban.com/":
 		            print 'login success !'
+	
+	def post_douban(self):
+		html = self.response.read()
+		#print html
+		ckcode = re.findall(r'<a href="http://www.douban.com/accounts/logout?.+ck=(.*?)">', html)
+		#print ckcode
+		#print type(ckcode[0])
+		content = raw_input("我说:")
+		dic = {
+			"ck":ckcode[0],
+			"comment":content
+		}
+
+		self.response = self.opener.open("http://www.douban.com/?", urllib.urlencode(dic))
+		if self.response.geturl() == "http://www.douban.com/":
+		    print 'post success !'
+				            
 
 email = raw_input("Email:")
 password = raw_input("Password:")
 m = LoginDouban(email, password)
 m.login_douban()
+m.post_douban()
+
